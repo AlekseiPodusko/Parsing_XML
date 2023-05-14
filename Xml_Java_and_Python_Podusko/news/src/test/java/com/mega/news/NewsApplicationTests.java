0036d9afafac0e5829234bd8e966ddb47e7e53ca
AlekseiPodusko <112948029+AlekseiPodusko@users.noreply.github.com>
2023-05-14 15:09:24 +0300

    Add files via upload

---
+++ Xml_Java_and_Python_Podusko/news/src/test/java/com/mega/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mega.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
